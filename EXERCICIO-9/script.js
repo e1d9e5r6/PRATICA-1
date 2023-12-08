e0bbb51ee44bf2e0e4e76ce983ccb49e6a6da0b7
e1d9e5r6 <eder@eabcomercio.com.br>
2023-12-08 15:34:31 -0300

    Rev-5

--- EXERCICIO-9/script.js
+++ EXERCICIO-9/script.js
@@ -4,20 +4,6 @@ menos do que uma dúzia, e R$0,25 se forem compradas pelo menos
 doze. Desenvolva um algoritmo que leia o número de maçãs
 compradas, calcule e escreva o valor total da compra.
 */
-/*
-const valorMinimo = number(prompt("Valor na quantidade compra menor que 12", 0.25));
-const valorMaximo = number(prompt("Valor na quantidade compra igual ou maior que 12", 0.30));
-
-let quantCompra = number(prompt("Inserir quantidade da compra",""));
-
-let valorCompra1 = (valorMinimo * quantCompra);
-
-let valorCompra2 = (valorMinimo * quantCompra);
-
-if(quantCompra < 12)
-alert("Compra com desconto");
-if(quantCompra >= 12)
-alert("Compra sem desconto");*/
 
 const valorMinimo = 0.25
 const valorMaximo = 0.30
