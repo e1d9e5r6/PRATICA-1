9a682920cc79b81dabe2ab9e409470466dacf863
e1d9e5r6 <eder@eabcomercio.com.br>
2023-12-08 11:19:59 -0300

    Rev3

---
+++ EXERCICIO-8/script.js
@@ -0,0 +1,14 @@
+/*
+8. Desenvolva um algoritmo para ler dois valores e imprimir uma das
+três mensagens a seguir:
+a. ‘Números iguais’, caso os números sejam iguais;
+b. ‘Primeiro é maior’, caso o primeiro seja maior que o segundo;
+c. ‘Segundo maior’, caso o segundo seja maior que o primeiro.
+*/
+
+let valorPrimeiro = Number(prompt("Digite o primeiro valor", ""));
+let valorSegundo = Number(prompt("Digite o segundo valor", ""));
+let valorDiferença = valorPrimeiro - valorSegundo;
+
+If (valorDiferença = 0);
+document.write("Primeiro valor é igual ao segundo valor");
