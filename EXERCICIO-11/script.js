8f48ec0094d3ac31021c0bcbc925b0f1edbd4401
e1d9e5r6 <eder@eabcomercio.com.br>
2023-12-08 16:52:35 -0300

    rev-8

---
+++ EXERCICIO-11/script.js
@@ -0,0 +1,7 @@
+/*11. Crie um algoritmo que armazene um número inteiro positivo, e gere
+um alerta com as seguintes mensagens:
+a. “Número é par!”, se o valor armazenado for par;
+b. “Número é impar!”, se o valor armazenado for ímpar;*/
+
+let numTeste = Number(prompt("Inserir número = ",""));
+let numDivide = numTeste/2;
